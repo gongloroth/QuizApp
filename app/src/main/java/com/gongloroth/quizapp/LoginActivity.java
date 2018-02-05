bf2cb4ed3860fe8d234755577859c40a0e2f560d
gongloroth <gnambence@gmail.com>
2018-02-05 20:08:21 +0100

    5th commit
    recheck

--- app/src/main/java/com/gongloroth/quizapp/LoginActivity.java
+++ app/src/main/java/com/gongloroth/quizapp/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.gongloroth.quizapp;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -21,22 +24,22 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         //Array for pictures
-        final int []imageArray={R.drawable.blade_runner,R.drawable.wonder,R.drawable.shape_of};
+        final int[] imageArray = {R.drawable.blade_runner, R.drawable.wonder, R.drawable.shape_of};
 
 
         //Picture slideshow
         final Handler handler = new Handler();
         Runnable runnable = new Runnable() {
-            int i=0;
+            int i = 0;
+
             public void run() {
 
                 ImageView imageView = (ImageView) findViewById(R.id.login_image);
                 imageView.setImageResource(imageArray[i]);
                 imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
                 i++;
-                if(i>imageArray.length-1)
-                {
-                    i=0;
+                if (i > imageArray.length - 1) {
+                    i = 0;
                 }
                 handler.postDelayed(this, 4000);  //for interval...
             }
@@ -45,19 +48,17 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     //Start MainActivity with the typed name
-    public void onLogin(View view){
+    public void onLogin(View view) {
 
         EditText nameText = (EditText) findViewById(R.id.name_text);
 
-        if(!isEmpty(nameText)) {
+        if (!isEmpty(nameText)) {
             setName(nameText.getText().toString());
             Intent intent = new Intent(this, MainActivity.class);
             intent.putExtra(USER_NAME, getName());
             startActivity(intent);
-        }
-        else
-        {
-            Toast.makeText(this,getText(R.string.enter_name), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getText(R.string.enter_name), Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -67,6 +68,20 @@ public class LoginActivity extends AppCompatActivity {
             return false;
 
         return true;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View view = getCurrentFocus();
+        if (view != null && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) && view instanceof EditText && !view.getClass().getName().startsWith("android.webkit.")) {
+            int scrcoords[] = new int[2];
+            view.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + view.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + view.getTop() - scrcoords[1];
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom())
+                ((InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((this.getWindow().getDecorView().getApplicationWindowToken()), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 
 
--- app/src/main/java/com/gongloroth/quizapp/MainActivity.java
+++ app/src/main/java/com/gongloroth/quizapp/MainActivity.java
@@ -6,7 +6,9 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -27,18 +29,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setUserName(getIntent().getExtras().getString("name"));
+
     }
 
     //Create final result message based on the calculation and
     //make Submit button invisible and Send Result button visible
-    public void onSubmit(View view){
+    public void onSubmit(View view) {
 
         calculateResult();
 
         Context context = getApplicationContext();
         StringBuilder sb = new StringBuilder();
         Formatter fmt = new Formatter(sb);
-        fmt.format("Well done, %s!\nYour final score is: %d points.",getUserName(),getFinalResult());
+        fmt.format("Well done, %s!\nYour final score is: %d points.", getUserName(), getFinalResult());
         setMessage(sb.toString());
         CharSequence text = getMessage();
         int duration = Toast.LENGTH_LONG;
@@ -46,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
 
-        Button submitButton = (Button)view;
+        Button submitButton = (Button) view;
         submitButton.setVisibility(View.GONE);
         submitButton.setClickable(false);
 
@@ -61,64 +64,61 @@ public class MainActivity extends AppCompatActivity {
     //Calculate the final result and make the Views red or green based on the answer
     //Green = right answer
     //Red = wrong answer
-    public void calculateResult(){
+    public void calculateResult() {
 
-        checkRadioView(R.id.radio_q1_c,R.id.card_radiogroup1);
-        checkRadioView(R.id.radio_q2_b,R.id.card_radiogroup2);
-        checkRadioView(R.id.radio_q3_a,R.id.card_radiogroup3);
-        checkRadioView(R.id.radio_q4_c,R.id.card_radiogroup4);
-        checkRadioView(R.id.radio_q5_b,R.id.card_radiogroup5);
-        checkRadioView(R.id.radio_q7_c,R.id.card_radiogroup7);
-        checkRadioView(R.id.radio_q8_b,R.id.card_radiogroup8);
-        checkRadioView(R.id.radio_q9_c,R.id.card_radiogroup9);
-        checkCheckView(R.id.q6_checkbox_1,R.id.q6_checkbox_3,R.id.card_radiogroup6);
-        checkEditView(R.id.edit_text_q10,R.id.card_edit_text10);
+        checkRadioView(R.id.radio_q1_c, R.id.card_radiogroup1);
+        checkRadioView(R.id.radio_q2_b, R.id.card_radiogroup2);
+        checkRadioView(R.id.radio_q3_a, R.id.card_radiogroup3);
+        checkRadioView(R.id.radio_q4_c, R.id.card_radiogroup4);
+        checkRadioView(R.id.radio_q5_b, R.id.card_radiogroup5);
+        checkRadioView(R.id.radio_q7_c, R.id.card_radiogroup7);
+        checkRadioView(R.id.radio_q8_b, R.id.card_radiogroup8);
+        checkRadioView(R.id.radio_q9_c, R.id.card_radiogroup9);
+        checkCheckView(R.id.q6_checkbox_1, R.id.q6_checkbox_3, R.id.card_radiogroup6);
+        checkEditView(R.id.edit_text_q10, R.id.card_edit_text10);
     }
 
     //Check if Radiogroup's answer is correct and change background color based on answer
-    public void checkRadioView(int radioButtonID, int answerCardID){
+    public void checkRadioView(int radioButtonID, int answerCardID) {
         RadioButton button = (RadioButton) findViewById(radioButtonID);
-        if (button.isChecked()){
+        if (button.isChecked()) {
             finalResult++;
             CardView answer = (CardView) findViewById(answerCardID);
             answer.setCardBackgroundColor(getResources().getColor(R.color.colorRight));
-        }
-        else{
+        } else {
             CardView answer = (CardView) findViewById(answerCardID);
             answer.setCardBackgroundColor(getResources().getColor(R.color.colorWrong));
         }
     }
 
     //Check if Checkbox's answer is correct and change background color based on answer
-    public void checkCheckView(int checkbuttonID1,int checkButtonID2, int answerCardID){
+    public void checkCheckView(int checkbuttonID1, int checkButtonID2, int answerCardID) {
 
         CheckBox answer1Question6 = (CheckBox) findViewById(checkbuttonID1);
         CheckBox answer2Question6 = (CheckBox) findViewById(checkButtonID2);
 
-        if (answer1Question6.isChecked() && answer2Question6.isChecked()){
+        if (answer1Question6.isChecked() && answer2Question6.isChecked()) {
             finalResult++;
             CardView answer = (CardView) findViewById(answerCardID);
             answer.setCardBackgroundColor(getResources().getColor(R.color.colorRight));
-        }
-        else{
+        } else {
             CardView answer = (CardView) findViewById(answerCardID);
             answer.setCardBackgroundColor(getResources().getColor(R.color.colorWrong));
         }
     }
 
     //Check if EditText's answer is correct and change background color based on answer
-    public void checkEditView(int editTextID, int answerCardID){
+    public void checkEditView(int editTextID, int answerCardID) {
 
         EditText editText = (EditText) findViewById(editTextID);
         String userInput = editText.getText().toString().toLowerCase();
         String correctAnswer = getText(R.string.q10_text).toString().toLowerCase();
 
-        if (userInput.equals(correctAnswer)){
+        if (userInput.equals(correctAnswer)) {
             finalResult++;
             CardView answer = (CardView) findViewById(answerCardID);
             answer.setCardBackgroundColor(getResources().getColor(R.color.colorRight));
-        }
-        else{
+        } else {
             CardView answer = (CardView) findViewById(answerCardID);
             answer.setCardBackgroundColor(getResources().getColor(R.color.colorWrong));
         }
@@ -137,10 +137,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Send button method
-    public void onSend(View view){
+    public void onSend(View view) {
 
         composeEmail("Quiz Result", getMessage());
 
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View view = getCurrentFocus();
+        if (view != null && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) && view instanceof EditText && !view.getClass().getName().startsWith("android.webkit.")) {
+            int scrcoords[] = new int[2];
+            view.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + view.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + view.getTop() - scrcoords[1];
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom())
+                ((InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((this.getWindow().getDecorView().getApplicationWindowToken()), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 
 
